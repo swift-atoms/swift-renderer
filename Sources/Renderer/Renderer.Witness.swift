72bb0960c742b5ae0855fb83c1b267b50c884225
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-09-07 22:20:59 +0200

    WIP

--- Sources/Renderer/Renderer.Witness.swift
+++ Sources/Renderer/Renderer.Witness.swift
@@ -2,13 +2,18 @@ extension Renderer {
     public struct Witness<Input: ~Copyable & ~Escapable, Context: ~Copyable & ~Escapable, Failure: Swift.Error>:
         Renderer.`Protocol`
     {
-        private let operation: (borrowing Input, inout Context) throws(Failure) -> Void
+        private let operation: (_ input: borrowing Input, _ context: inout Context) throws(Failure) -> Void
 
-        public init(_ operation: @escaping (borrowing Input, inout Context) throws(Failure) -> Void) {
+        public init(
+            _ operation: @escaping (_ input: borrowing Input, _ context: inout Context) throws(Failure) -> Void
+        ) {
             self.operation = operation
         }
 
-        public borrowing func render(_ input: borrowing Input, into context: inout Context) throws(Failure) {
+        public borrowing func render(
+            _ input: borrowing Input,
+            into context: inout Context
+        ) throws(Failure) {
             try operation(input, &context)
         }
     }
